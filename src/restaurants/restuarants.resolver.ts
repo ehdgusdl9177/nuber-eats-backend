0a4ec2d8d917ad52f4b3367d956c941763c7e483
ehdgusdl9177 <k23129177@gmail.com>
2022-03-14 15:47:59 +0900

    ObjectType

--- src/restaurants/restuarants.resolver.ts
+++ src/restaurants/restuarants.resolver.ts
@@ -1,9 +1,10 @@
 import { Query, Resolver } from '@nestjs/graphql';
+import { Restuarant } from './entities/restuarant.entity';
 
-@Resolver()
+@Resolver(() => Restuarant)
 export class RestuarantResolver {
-  @Query(() => Boolean)
-  isPizzaGood() {
+  @Query(() => Restuarant)
+  myRestuarant() {
     return true;
   }
 }
