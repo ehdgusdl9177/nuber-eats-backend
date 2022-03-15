cbd20af84178655e36c77b515092b27f6f27f44f
ehdgusdl9177 <k23129177@gmail.com>
2022-03-15 15:24:32 +0900

    ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,16 @@
 import { ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot({
-      driver: ApolloDriver,
-      autoSchemaFile: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
     }),
-    RestaurantsModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
@@ -21,6 +21,11 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
       synchronize: true,
       logging: true,
     }),
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+    RestaurantsModule,
   ],
   controllers: [],
   providers: [],
