ef08b422b9cdc368b38626a471a07209e3607ccc
ehdgusdl9177 <k23129177@gmail.com>
2022-03-15 15:13:24 +0900

    TypeORM Setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
@@ -10,6 +11,16 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
       autoSchemaFile: true,
     }),
     RestaurantsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'hyeon',
+      password: 'kim9811',
+      database: 'nuber-eats',
+      synchronize: true,
+      logging: true,
+    }),
   ],
   controllers: [],
   providers: [],
