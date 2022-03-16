f2374359f1699f19573924539d7f56dc6806b845
ehdgusdl9177 <k23129177@gmail.com>
2022-03-16 15:46:43 +0900

    Update Restaurant Part One

---
+++ src/restaurants/dtos/update-restaurant.dto.ts
@@ -0,0 +1,16 @@
+import { ArgsType, Field, InputType, PartialType } from '@nestjs/graphql';
+import { CreateResturantDto } from './create-restuarant.dto';
+
+@InputType()
+export class UpdateRestaurantInputType extends PartialType(
+  CreateResturantDto,
+) {}
+
+@InputType()
+export class UpdateRestaurantDto {
+  @Field(() => Number)
+  id: number;
+
+  @Field(() => UpdateRestaurantInputType)
+  data: UpdateRestaurantInputType;
+}
