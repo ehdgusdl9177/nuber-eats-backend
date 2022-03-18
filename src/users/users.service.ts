456e794cb1b83cf521f357b6af4a3c4d5d6d0fc6
ehdgusdl9177 <k23129177@gmail.com>
2022-03-18 12:01:48 +0900

    Log In part Two

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateAccountInput } from './dtos/create-account-dto';
+import { LoginInput } from './dtos/login.dto';
 import { User } from './entites/user.entity';
 
 @Injectable()
@@ -14,17 +15,52 @@ export class UsersService {
     email,
     password,
     role,
-  }: CreateAccountInput): Promise<[boolean, string?]> {
+  }: CreateAccountInput): Promise<{ ok: boolean; error?: string }> {
     try {
       const exists = await this.users.findOne({ email });
       if (exists) {
-        return [false, 'There is a user with that email already'];
+        return { ok: false, error: 'There is a user with that email already' };
       }
       await this.users.save(this.users.create({ email, password, role }));
-      return [true];
+      return { ok: true };
     } catch (e) {
-      return [false, "Couldn't create account"];
+      return { ok: false, error: "Couldn't create account" };
     }
     // create user & hash the password
+  }
+
+  async login({
+    email,
+    password,
+  }: LoginInput): Promise<{ ok: boolean; error?: string; token?: string }> {
+    // find the user with the email
+    // check if the password is correct
+    // make a JWT and give it to the user
+
+    try {
+      const user = await this.users.findOne({ email });
+      if (!user) {
+        return {
+          ok: false,
+          error: 'User not found',
+        };
+      }
+      const passwordCorrect = await user.checkPassword(password);
+      if (!passwordCorrect) {
+        return {
+          ok: false,
+          error: 'Wrong password',
+        };
+      }
+      return {
+        ok: true,
+        token: 'lalalal',
+      };
+    } catch (error) {
+      return {
+        ok: false,
+        error,
+      };
+    }
   }
 }
