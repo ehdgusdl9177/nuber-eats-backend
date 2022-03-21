bb66846e25f990dbbe90bcebc0a6335539c886f4
ehdgusdl9177 <k23129177@gmail.com>
2022-03-21 15:35:07 +0900

    AuthUser Decorator

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,5 +1,6 @@
 import { UseGuards } from '@nestjs/common';
 import { Args, Context, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { AuthUser } from 'src/auth/auth-user.decorator';
 import { AuthGuard } from 'src/auth/auth.guard';
 import {
   CreateAccountInput,
@@ -47,5 +48,7 @@ export class UsersResolver {
 
   @Query(() => User)
   @UseGuards(AuthGuard)
-  me() {}
+  me(@AuthUser() authUser: User) {
+    return authUser;
+  }
 }
